4a88ff7cc764c93af8bc4ba4454ebf351c994322
Obed Bigirimana <obedbigirimana@gmail.com>
2017-01-24 14:15:36 +0200

    Added codes about deleting an item

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -5,8 +5,48 @@ class StaffsController < ApplicationController
     render "first_staff.html.erb"
   end 
 
-  def all_staff
+  def index
     @all_staff = Staff.all 
-    render "all_staff.html.erb"
+    render "index.html.erb"
   end 
+
+  def show
+    @staffs = Staff.find_by(id: params[:id])
+  end 
+
+  def new
+  end 
+
+  def create
+    name = params[:name]
+    organization = params[:organization]
+    email = params[:email]
+    phone_number = params[:phone_number]
+    staffs= Staff.new({name: name, organization: organization, email: email, phone_number: phone_number })
+    staffs.save
+  end 
+
+  def edit
+    @staffs = Staff.find_by(id: params[:id])
+  end 
+
+  def update
+    @staffs = Staff.find_by(id: params[:id])
+    @staffs.name = params[:name]
+    @staffs.organization = params[:organization]
+    @staffs.email = params[:email]
+    @staffs.phone_number = params[:phone_number]
+    @staffs.save 
+
+  end
+
+  def destroy
+    @member = Staff.find_by(id: params[:id])
+    @member.delete
+    render "destroy"
+
+  end 
+
 end
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
- get "/first_staff", to: "staffs#first_staff"
- get "/all_staff", to: "staffs#all_staff"
+ get "/staffs", to: "staffs#index"
+ get "/staffs", to: "staffs#index"
+ get "/staffs/new", to: 'staffs#new'
+ get "/staffs/:id", to: 'staffs#show'
+ post "/staffs", to: 'staffs#create'
+ get "/staffs/:id/edit", to: 'staffs#edit'
+ patch "/staffs/:id", to: 'staffs#update'
+ delete "/staffs/:id", to: 'staffs#destroy'
 end
 
 
 
+#   get "/recipes/:id", to: 'recipes#show'
+# end
+
+
+
+#  get "/recipes", to: 'recipes#index'
+#   get "/recipes/new", to: 'recipes#new'
+#   get "/recipes/:id", to: 'recipes#show'
+#   post "/recipes", to: 'recipes#create'
+#   get "/recipes/:id/edit", to: 'recipes#edit'
+# patch "/recipes/:id",to: 'recipes#update'
